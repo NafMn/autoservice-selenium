da4e7690111f0d3c3ecdde71157a2521cdc94cd0
GitNaf <114709801+NafMn@users.noreply.github.com>
2024-05-08 12:09:03 +0700

    fix : bug sampai 4

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 from openpyxl import load_workbook
 
 import time
 
-micro = load_workbook(filename="D:\Aplikasi\micronook.xlsx")
-
+micro = load_workbook(filename="D:\\Aplikasi\\micronook.xlsx")
 sheetrange = micro['Sheet1']
-
 
 driver = webdriver.Chrome()
 
@@ -20,87 +18,78 @@ driver.get("https://www.micromentor.org/e-learning/seri-literasi-keuangan/pelati
 driver.maximize_window()
 driver.implicitly_wait(10)
 
-# looping
 
 i = 5
 
-# len(sheetrange['A'])
-while i <=6:
-    Email = sheetrange['A'+str(i)].value
-    Password = sheetrange['B'+str(i)].value
-    Nama = sheetrange['C'+str(i)].value
+while i <= 6:
+    Email = sheetrange['A' + str(i)].value
+    Password = sheetrange['B' + str(i)].value
+    Nama = sheetrange['C' + str(i)].value
 
+
+    # Close the cookie dialog if present
+    try:
+        cookie_dialog = driver.find_element(By.ID, "CybotCookiebotDialog")
+        if cookie_dialog.is_displayed():
+            close_button = cookie_dialog.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinDecline")
+            close_button.click()
+    except:
+        pass
     button_masuk = driver.find_element(By.XPATH, '//a[@class="button primary hollow"]')
     button_masuk.click()
 
     try:
-        WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="main-content"]/div/section[2]')))
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]/div/section[2]')))
 
-        driver.find_element_by_id('id_username').send_keys(Email)
-        driver.find_element_by_id('id_password').send_keys(Password)
-        button_sub = driver.find_element_by_id('submit')
+        driver.find_element(By.ID,'id_username').send_keys(Email)
+        driver.find_element(By.ID,'id_password').send_keys(Password)
+        button_sub = driver.find_element(By.XPATH, '//button[@type="submit"]')
         button_sub.click()
 
-        # Menunggu halaman selanjutnya dimuat
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]/div/div/div/div/a')))
 
-        # Klik halaman selanjutnya
-        next_1 = driver.find_element_by_css_selector('a.button.primary.hollow')
-        # Menggunakan link text
-        # driver.find_element_by_link_text('Mulai Pelatihan').click()
+        next_1 = driver.find_element(By.XPATH,'//a[@class="button primary"]')
         next_1.click()
 
-
-        # Menunggu halaman selanjutnya dimuat
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]/div[2]/div/div/a')))
-
-        # xpath
-        driver.find_element(By.XPATH,'//a[@class="button primary training-nav__next"]').click()
-        # Menggunakan link text
-        # driver.find_element_by_link_text('Lanjutkan').click()
-
-        # Menunggu halaman selanjutnya dimuat
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]/div[1]/form/div[1]')))
 
-        driver.find_element_by_id('id_answer_1')
-        driver.find_element_by_id('submit').click()
+        jawaban1 = driver.find_element(By.ID,'id_answer_1')
+        jawaban1.click()
+        button_sub = driver.find_element(By.XPATH, '//button[@type="submit"]')
+        button_sub.click()
 
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]')))
         
-        # Menggunakan link text
         driver.find_element_by_link_text('Lanjutkan').click()
-        # css
-        # driver.find_element_by_css_selector('a.button.primary.training-nav__next').click()
-
 
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]/div[1]/form')))
 
-        driver.find_element_by_id('id_answer_0')
+        jawaban2 = driver.find_element(By.ID,'id_answer_0')
+        jawaban2.click()
         driver.find_element_by_id('submit').click()
 
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]')))
         
-        # Menggunakan link text
         driver.find_element_by_link_text('Lanjutkan').click()
 
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]/div[1]/form')))
         
-        driver.find_element_by_id('id_answer_3')
+        jawaban3 = driver.find_element(By.ID,'id_answer_3')
+        jawaban3.click()
         driver.find_element_by_id('submit').click()
 
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]')))
         
-        # Menggunakan link text
         driver.find_element_by_link_text('Lanjutkan').click()
 
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]/div[1]/form')))
         
-        driver.find_element_by_id('id_answer_3')
+        jawaban4 = driver.find_element(By.ID,'id_answer_3')
+        jawaban4.click()
         driver.find_element_by_id('submit').click()
 
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]')))
         
-        # Menggunakan link text
         driver.find_element_by_link_text('Lanjutkan').click()
 
     except TimeoutException:
@@ -108,9 +97,8 @@ while i <=6:
         pass
     
     time.sleep(5)
-    i = i+1
+    i += 1
 
-print("oke")
+print("Oke")
 
-# Tutup WebDriver
 driver.quit()
